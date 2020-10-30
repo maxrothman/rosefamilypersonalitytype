bc0eef3a07c5587b4fbd78c908c36eaacd811628
Dot Levine <them@dottie.me>
2020-10-29 22:43:31 -0400

    plugging along with Max

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
 import json
+from pathlib import Path
 
-with open("questions/question1.json") as f:
-    questiondata = json.loads(f.read())
-    print(questiondata["question"])
+# inventory each file in question folder and create function which calls each question
+
+questionPath = (Path('questions/'))
+questionCount = len(list(questionPath.iterdir()))
+for path in sorted(questionPath.iterdir()):
+    with open(path) as f:
+        questionData = json.loads(f.read())
+        print(questionData["question"])
+        # could use an ARRAY of objects instead
+        number = 1
+        for answer in questionData["answers"].keys():
+            print(str(number) + ". " + answer)
+            number = number+1
+        prompt = "Input answer number: "
+        while True:
+            choice = input(prompt)
+            prompt = "Quit fucking around, gosh. "
+            try:
+                choice = int(choice)
+            except ValueError:
+                continue
+            if 0 < choice < len(questionData["answers"]):
+                break
+
+# prepend each answer option with letter in alphabetical order, and print
