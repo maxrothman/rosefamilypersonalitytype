3f5b2080de296f7bd16995c34dddabb3d257a70d
Dot Levine <them@dottie.me>
2020-10-30 18:46:36 -0400

    working on creating scores

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 scores = []
 
-questionPath = (Path('questions/'))
+questionPath = Path('questions/')
 questionCount = len(list(questionPath.iterdir()))
 for path in sorted(questionPath.iterdir()):
     with open(path) as f:
@@ -27,8 +27,8 @@ for path in sorted(questionPath.iterdir()):
             continue
         if 0 < choice < len(questionData["answers"]) + 1:
             break
-    answerStr = answers[choice-1]
-    scores.append(questionData["answers"][answerStr])
+answerStr = answers[choice-1]
+scores.append(questionData["answers"][answerStr])
 scoreTotal = {
         "lovability": 0,
         "selfishness": 0,
@@ -43,5 +43,13 @@ for index in range(len(scores)):
         scoreTotal[key] = scoreTotal[key] + value
 for stat, value in scoreTotal.items():
     # what the fuck is going on here; why does scoreTotal[stat] output it's value
-    scoreTotal[stat] = value / questionCount
-print(scoreTotal)
+    scoreTotal[stat] = round(value / questionCount)
+characterPath = Path("characters/")
+for charFile in characterPath.iterdir():
+    with open(charFile) as f:
+        characterData = json.loads(f.read())
+    # for each character file create score for that character based on calculations
+    tempName = str("score" + characterData["name"])
+    exec("%s = %d" % (tempName,0))
+    # for key, value in characterData.items():
+        # print(str(characterData["name"] + "score"))
