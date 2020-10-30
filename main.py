23f0717cfe72b01b714e570faf8640150d5ae6b6
Dot Levine <them@dottie.me>
2020-10-30 00:10:45 -0400

    ready to start calculating

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ from pathlib import Path
 
 # inventory each file in question folder and create function which calls each question
 
+scores = []
+
 questionPath = (Path('questions/'))
 questionCount = len(list(questionPath.iterdir()))
 for path in sorted(questionPath.iterdir()):
     with open(path) as f:
         questionData = json.loads(f.read())
-        print(questionData["question"])
-        # could use an ARRAY of objects instead
-        number = 1
-        for answer in questionData["answers"].keys():
-            print(str(number) + ". " + answer)
-            number = number+1
-        prompt = "Input answer number: "
-        while True:
-            choice = input(prompt)
-            prompt = "Quit fucking around, gosh. "
-            try:
-                choice = int(choice)
-            except ValueError:
-                continue
-            if 0 < choice < len(questionData["answers"]):
-                break
+    print(questionData["question"])
+    # could use an ARRAY of objects instead
+    number = 1
+    answers = list(questionData["answers"].keys())
+    for answer in answers:
+        print(str(number) + ". " + answer)
+        number = number+1
+    prompt = "Input answer number: "
+    while True:
+        choice = input(prompt)
+        prompt = "Quit fucking around, gosh. "
+        try:
+            choice = int(choice)
+        except ValueError:
+            continue
+        if 0 < choice < len(questionData["answers"]):
+            break
+    answerStr = answers[choice-1]
+    scores.append(questionData["answers"][answerStr])
 
-# prepend each answer option with letter in alphabetical order, and print
+
